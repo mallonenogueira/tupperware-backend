1be9ca46c6a151a297279250e3e9cd3a0b55e999
Mallone <mallonenogueira@gmail.com>
2020-02-13 19:37:10 -0300

    Add front-end build

---
+++ public/precache-manifest.3eb08b3e039edfc156ba1a9ed00699ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eed07e4aaa566faa663e",
+    "url": "/tupper-app/static/css/main.b7795d50.chunk.css"
+  },
+  {
+    "revision": "eed07e4aaa566faa663e",
+    "url": "/tupper-app/static/js/main.eed07e4a.chunk.js"
+  },
+  {
+    "revision": "a711f16f4a0a13896440",
+    "url": "/tupper-app/static/js/1.a711f16f.chunk.js"
+  },
+  {
+    "revision": "64146ae14a69a47a2ac0",
+    "url": "/tupper-app/static/js/runtime~main.64146ae1.js"
+  },
+  {
+    "revision": "e453c7dba4cfa1ef4cfb5da574be5918",
+    "url": "/tupper-app/index.html"
+  }
+];
